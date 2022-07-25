97fa99b6dd7c75e1ce0241b3c7fea50229a8ab80
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2022-07-25 18:15:09 +0700

    chore: remove template repo comment

--- src/msg.rs
+++ src/msg.rs
@@ -21,7 +21,6 @@ pub enum ExecuteMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
     Config {},
     GasPrice {
         token: String,
